f0edc2c7fa2f6c5a2dc2f88abee01977fc9eb059
Yana <holoborodkoyana97@gmail.com>
2020-11-22 19:37:22 +0100

    A starter for Delivery service smart contract

---
+++ Delivery.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.5.12;
+
+contract Delivery {
+
+    struct Package {
+        uint id,
+        address sender,
+        address receiver,
+        uint deliveryCost,
+        string status
+    }
+
+}
