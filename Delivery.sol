ac5340dbbe03efc9b69a95bde8a2348b4fc5c570
Yana <holoborodkoyana97@gmail.com>
2020-11-23 10:17:15 +0100

    createPackage function

--- Delivery.sol
+++ Delivery.sol
@@ -1,14 +1,31 @@
 import './Ownable.sol';
 pragma solidity 0.5.12;
 
-contract Delivery {
+contract Delivery is Ownable {
 
     struct Package {
-        uint id,
-        address sender,
-        address receiver,
-        uint deliveryCost,
-        string status
+        uint id;
+        address sender;
+        address receiver;
+        uint deliveryCost;
+        string status;
+        bool isPaid;
+    }
+
+    mapping(uint => Package) private packages;
+
+    function createPackage(uint id, address sender, address receiver, uint deliveryCost) public onlyOwner {
+        Package memory newPackage;
+
+        //temporary solution for id
+        newPackage.id = id;
+        newPackage.sender = sender;
+        newPackage.receiver = receiver;
+        newPackage.deliveryCost = deliveryCost;
+        newPackage.status = 'new';
+        newPackage.isPaid = false;
+
+        packages[id] = newPackage;
     }
 
 }
